76ef473358c807742ead161f88837c52b7504269
JWHCode <45348787+JWHCode@users.noreply.github.com>
2022-03-23 15:18:40 -0400

    Update CodeWars.sql

--- CodeWars.sql
+++ CodeWars.sql
@@ -7,3 +7,32 @@
   -- copies_sold
   
   SELECT name, author, copies_sold from books ORDER BY copies_sold DESC LIMIT 5
+
+
+--For this challenge you need to create a SELECT statement that will contain data about departments that had a sale with a price over 98.00 dollars. This SELECT statement will have to use an EXISTS to achieve the task.
+/*
+departments table schema
+
+    id
+    name
+
+sales table schema
+
+    id
+    department_id (department foreign key)
+    name
+    price
+    card_name
+    card_number
+    transaction_date
+
+resultant table schema
+
+    id
+    name
+
+*/
+
+SELECT departments.id, departments.name FROM departments WHERE EXISTS 
+(SELECT sales.department_id, departments.id, sales.price, departments.name WHERE sales.department_id = departments.id AND sales.price > 98.00)
+GROUP BY departments.id
